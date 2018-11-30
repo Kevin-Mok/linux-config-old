8ca4661cb2685b548dd2bfb9afd144159012066b
Kevin Mok <kevin.mok@live.ca>
2018-11-30 00:03:55 -0500

    Set key dirs

--- shortcuts.sh
+++ shortcuts.sh
@@ -22,9 +22,9 @@ ensure "source $shell_shortcuts" "$shellrc"
 ensure "source $HOME/.config/ranger/shortcuts.conf" "$HOME/.config/ranger/rc.conf"
 
 # Format the `folders` file in the correct syntax and sent it to all three configs.
-sed "s/#.*$//;/^$/d" "$folders" | tee >(awk '{print $1"=\"cd "$2" && ls -a\" \\"}' >> "$shell_shortcuts") \
+sed "s/#.*$//;/^$/d" "$folders" | tee >(awk '{print $1"=\"f "$2"\" \\"}' >> "$shell_shortcuts") \
 	| awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> "$ranger_shortcuts"
 
 # Format the `configs` file in the correct syntax and sent it to both configs.
-sed "s/#.*$//;/^$/d"  "$configs" | tee >(awk '{print $1"=\"$EDITOR "$2"\" \\"}' >> "$shell_shortcuts") \
-	| awk '{print "map "$1" shell $EDITOR "$2}' >> "$ranger_shortcuts"
+# sed "s/#.*$//;/^$/d"  "$configs" | tee >(awk '{print $1"=\"$EDITOR "$2"\" \\"}' >> "$shell_shortcuts") \
+	# | awk '{print "map "$1" shell $EDITOR "$2}' >> "$ranger_shortcuts"
