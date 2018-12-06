826c320851f0afa06f465cc9fed698b86461eb40
Kevin Mok <kevin.mok@live.ca>
2018-12-05 21:31:21 -0500

    Simplified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# vars {{{ #
-
 shopt -s dotglob
 
 lc_dir="/home/kevin/linux-config"
@@ -10,45 +8,25 @@ link_to_lc () {
 	echo "Linking $1 from $3 to $2."
 	sys_dir="$2"
 	config_dir="$3"
+	items=()
 	case "$1" in
-
-# }}} vars #
-
-		# files {{{ #
-		
 		files)
-			for file in "$config_dir"/*; do
-				file_name=$(basename -- "$file")
-				# echo "$file_name"
-				# Remove system file and link config in repository to system.
-				rm -f "${sys_dir:?}"/"$file_name"
-				ln -s "$config_dir"/"$file_name" "$sys_dir"/"$file_name" && echo "Linked $file_name."
-			done ;;
-		
-		# }}} files #
-
-		# dirs {{{ #
-		
+			items="$config_dir"/* ;;
 		dirs)
-			for dir in "$config_dir"/*/; do
-				dir_name=$(basename -- "$dir")
-				# echo "$dir_name"
-				rm -f "${sys_dir:?}"/"$dir_name"
-				ln -s "$config_dir"/"$dir_name" "$sys_dir"/"$dir_name" && echo "Linked $dir_name."
-			done ;;
-		
-		# }}} dirs  #
-
-# run commands {{{ #
-
+			items="$config_dir"/*/ ;;
 	esac
+	for item in $items; do
+		item_name=$(basename -- "$item")
+		# echo "$item_name"
+		# Remove system item and link config in repository to system.
+		rm -f "${sys_dir:?}"/"$item_name"
+		ln -s "$config_dir"/"$item_name" "$sys_dir"/"$item_name" && echo "Linked $item_name."
+	done
 }
 
 link_to_lc "files" "$HOME" "$lc_dir/dotfiles"
 link_to_lc "dirs" "$HOME/.config" "$lc_dir/configs"
 link_to_lc "dirs" "$HOME/.themes" "$lc_dir/gtk/themes"
-
-# }}} run #
 
 # setup nvim config {{{ #
 
